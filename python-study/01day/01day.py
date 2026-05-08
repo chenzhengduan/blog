a5262ae7c228d31befe82d5f1e7e8fdba4c34caa
code.duan <code.duan@gmail.com>
2026-05-08 17:29:51 +0800

    feat;

---
+++ python-study/01day/01day.py
@@ -0,0 +1,35 @@
+# print("hello world")
+
+# float = 1.5
+# print(float)
+
+# bool = True
+# print(bool)
+# float = bool;
+# print(float)
+# float=None
+# print(float)
+# print(False==0)
+# print("123"==123)
+# print("1"==1,"1"==str(1))
+# print(1==int('1'))
+
+# for   i in range(5):
+#     print(i)    
+
+# true=True
+
+# num=1==1 and 2==3
+# print(num)
+
+# age=123
+# print(age)
+
+# str='123'
+# print(type(str)==type('1'))
+
+
+for i in range(1, 10):
+    for j in range(1, i + 1):
+        print(f"{j}×{i}={i*j}", end="\t")
+    print()
